020350d61f53a5ad621e66bcd0d8ee4fbdb5c4b3
robopoint <robo.peregrin.took@gmail.com>
2024-06-14 15:15:35 -0500

    Adding result videos + carousel

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var options = {
         slidesToScroll: 1,
-        slidesToShow: 3,
+        slidesToShow: 1,
         loop: true,
         infinite: true,
         autoplay: false,
